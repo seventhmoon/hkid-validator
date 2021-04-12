3a09ec8c0e4af93bfa70a41fff7431a79d98d00f
Fung <fung@androidfung.com>
2021-04-12 11:55:33 +0800

    added test cases

--- lib/src/androidTest/java/com/androidfung/validator/lib/ExampleInstrumentedTest.kt
+++ lib/src/androidTest/java/com/androidfung/validator/lib/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package com.androidfung.validator.lib
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +19,19 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.androidfung.validator.lib.test", appContext.packageName)
+    }
+
+    @ExperimentalStdlibApi
+    @Test
+    fun hkidTest() {
+        assert(HkidValidator.isValid("A123456", '3'))
+        assertFalse(HkidValidator.isValid("A123456", '4'))
+
+        assert(HkidValidator.isValid("A654321", '1'))
+        assertFalse(HkidValidator.isValid("A654321", 'A'))
+
+        assert(HkidValidator.isValid("ZA654321", 'A'))
+        assertFalse(HkidValidator.isValid("ZA654321", '9'))
+
     }
 }
--- lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
+++ lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
@@ -1,30 +1,31 @@
 package com.androidfung.validator.lib
 
 class HkidValidator {
-    private val acceptedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
-    private val hkidRegex = Regex("[A-Z]{1,2}\\d{6}")
 
-    @ExperimentalStdlibApi
-    fun isValid(id: String, checksum: Char) : Boolean{
-        return try {
-            calcCheckDigit(id) == checksum
-        }catch (e : IllegalArgumentException){
-            false
+    companion object {
+        private val acceptedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+        private val hkidRegex = Regex("[A-Z]{1,2}\\d{6}")
+
+        @ExperimentalStdlibApi
+        fun isValid(id: String, checksum: Char): Boolean {
+            return try {
+                calcCheckDigit(id) == checksum
+            } catch (e: IllegalArgumentException) {
+                false
+            }
         }
-    }
 
-    @ExperimentalStdlibApi
-    fun calcCheckDigit(input: String): Char {
-        if (input.matches(hkidRegex)) {
-            val id = if (input.length == 7) " $input" else input
-            val sum = id.mapIndexed { index, c ->
-                (9 - index) * acceptedChars.indexOf(c)
-            }.sum()
+        @ExperimentalStdlibApi
+        fun calcCheckDigit(input: String): Char {
+            if (input.matches(hkidRegex)) {
+                val id = if (input.length == 7) " $input" else input
+                val sum = id.mapIndexed { index, c ->
+                    (9 - index) * acceptedChars.indexOf(c)
+                }.sum()
+                val checksum = ((11 - (sum % 11)) % 11)
 
-            val checksum = ((11 - (sum % 11)) % 11) // = (11 * ceil(sum / 11.0) - sum).toInt()
-
-            return checksum.digitToChar(16)
-
-        } else throw IllegalArgumentException()
+                return checksum.digitToChar(16)
+            } else throw IllegalArgumentException()
+        }
     }
 }
