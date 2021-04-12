e5bad8c60e58dcd627666696aeb730a4ced86f30
Fung <fung@androidfung.com>
2021-04-12 09:33:23 +0800

    clean up

--- lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
+++ lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
@@ -1,19 +1,22 @@
 package com.androidfung.validator.lib
 
+import kotlin.math.ceil
+
 class HkidValidator {
     private val acceptedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
     private val hkidRegex = Regex("[A-Z]{1,2}\\d{6}")
 
     @ExperimentalStdlibApi
     fun calcCheckDigit(input: String): Char {
-        return if (input.matches(hkidRegex)) {
+        if (input.matches(hkidRegex)) {
             val id = if (input.length == 7) " $input" else input
             val sum = id.mapIndexed { index, c ->
                 (9 - index) * acceptedChars.indexOf(c)
             }.sum()
 
-            val checksum = (11 - (sum % 11) % 11)
-            checksum.digitToChar(16)
+            val checksum = (11 * ceil(sum / 11.0).toInt() - sum)
+
+            return checksum.digitToChar(16)
 
         } else throw IllegalArgumentException()
     }
