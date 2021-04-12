4c172ee526f40977d604d787723d027c0509a7fe
Fung <fung@androidfung.com>
2021-04-12 08:55:28 +0800

    clean up

--- lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
+++ lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
@@ -1,7 +1,6 @@
 package com.androidfung.validator.lib
 
 class HkidValidator {
-
     private val acceptedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
     private val hkidRegex = Regex("[A-Z]{1,2}\\d{6}")
 
@@ -10,16 +9,12 @@ class HkidValidator {
         return if (input.matches(hkidRegex)) {
             val id = if (input.length == 7) " $input" else input
             val sum = id.mapIndexed { index, c ->
-                (9 - index) * c.toMultiplier()
+                (9 - index) * acceptedChars.indexOf(c)
             }.sum()
 
             val checksum = (11 - (sum % 11) % 11)
             checksum.digitToChar(16)
 
         } else throw IllegalArgumentException()
-    }
-
-    private fun Char.toMultiplier(): Int {
-        return acceptedChars.indexOf(this, ignoreCase = true)
     }
 }
