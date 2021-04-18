5a9f5162f6404aa0aa91c21effba940a15b8c786
Fung <fung@androidfung.com>
2021-04-18 12:08:56 +0800

    clean up

--- lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
+++ lib/src/main/java/com/androidfung/validator/lib/HkidValidator.kt
@@ -3,10 +3,10 @@ package com.androidfung.validator.lib
 class HkidValidator {
 
     companion object {
-        private const val ACCEPTED_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+        private const val CHAR_LIST = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ "
         private val hkidRegex = Regex("[A-Z]{1,2}\\d{6}")
 
-        @ExperimentalStdlibApi
+        @JvmStatic
         fun isValid(id: String, checksum: Char): Boolean {
             return try {
                 calcCheckDigit(id) == checksum
@@ -15,12 +15,12 @@ class HkidValidator {
             }
         }
 
-        @ExperimentalStdlibApi
+        @JvmStatic
         fun calcCheckDigit(input: String): Char {
             if (input.matches(hkidRegex)) {
                 val id = if (input.length == 7) " $input" else input
                 val sum = id.mapIndexed { index, c ->
-                    (9 - index) * ACCEPTED_CHARS.indexOf(c)
+                    (9 - index) * CHAR_LIST.indexOf(c)
                 }.sum()
                 val checksum = ((11 - (sum % 11)) % 11)
 
